b39cf0db92aae71ae18a271fe1c85ba7a6e32d25
Mark Andrews <mjandrews.org@gmail.com>
2021-04-22 13:16:41 +0100

    Add script2.R

---
+++ scripts/script2.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+
+
+blp_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/dwrs02/master/data/blp-trials-short.txt")
+
+
+# summarize ---------------------------------------------------------------
+
+summarise(blp_df, avg = mean(rt, na.rm = T))
+          
+summarise(blp_df, 
+          avg_rt = mean(rt, na.rm = T),
+          sd_rt = sd(rt, na.rm = T),
+          median_raw = median(rt.raw, na.rm = T),
+          mad_raw = mad(rt.raw, na.rm = T)
+)
+          
+summarise(blp_df, 
+          avg_rt = mean(rt, na.rm = T),
+          sd_rt = sd(rt, na.rm = T),
+          median_raw = median(rt.raw, na.rm = T),
+          mad_raw = mad(rt.raw, na.rm = T),
+          range_rt = range(rt)
+)
+
+# count number of unique values in each variable
+summarise(blp_df, across(everything(), n_distinct))
+
+# calculate mean of last three variables
+summarise(blp_df, across(rt:rt.raw, mean))
+
+mean_xna <- function(x) mean(x, na.rm = T)
+
+summarise(blp_df, across(rt:rt.raw, mean_xna))
+
+summarise(blp_df, across(rt:rt.raw, ~mean(., na.rm = T)))
+
+list(a = 12, b = 2, c = 3)
+c(a = 12, b = 2, c = 3)
+
+list(avg = ~mean(., na.rm = T),
+     stdev = ~sd(., na.rm = T)
+)
+
+summarise(blp_df,
+          across(rt:rt.raw,
+                 list(avg = ~mean(., na.rm = T),
+                      stdev = ~sd(., na.rm = T)
+                 )
+          )
+)
+
+# split apply combine
+group_by(blp_df, lex) %>% 
+  summarize(avg = mean(rt, na.rm = T))
+
+summarize(group_by(blp_df, lex),
+          avg = mean(rt, na.rm = T))
+
+group_by(blp_df, lex, resp) %>% 
+  summarize(avg = mean(rt, na.rm = T))
+
+group_by(blp_df, lex) %>% 
+  summarise(across(rt:rt.raw, ~mean(., na.rm = T)))
+
+group_by(blp_df, participant) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n))
